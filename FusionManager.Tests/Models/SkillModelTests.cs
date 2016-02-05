bdcf5a6a41fd4941211aede43a4acbf99eac390e
lchesley <lisachesley@gmail.com>
2016-02-05 18:11:50 -0500

    Add Skill Model
    Add the Skill Model and associated tests.

---
+++ FusionManager.Tests/Models/SkillModelTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FusionManager.Models;
+using System.IO;
+using System.Web;
+
+
+namespace FusionManager.Tests.Models
+{
+    [TestClass]
+    public class SkillModelTests
+    {
+        SkillModel model;
+
+        [TestInitialize]
+        [DeploymentItem(@"App_Data\", @"App_Data\")]
+        public void Initialize()
+        {
+            StreamReader reader = new StreamReader("App_Data\\SkillList.csv");
+            model = new SkillModel(reader);
+        }
+
+        [TestMethod]
+        public void GetSkillList()
+        {
+            //Arrange                
+
+            //Act
+            var result = model.GetSkillList();
+
+            //Assert
+            Assert.IsNotNull(result);
+            CollectionAssert.AllItemsAreNotNull(result);
+        }
+
+        [TestMethod]
+        public void GetSkillList_CountGreaterThanZero()
+        {
+            //Arrange                
+            int count = 0;
+
+            //Act
+            var result = model.GetSkillList();
+
+            //Assert
+            Assert.AreNotEqual(count, result.Count);
+            CollectionAssert.AllItemsAreNotNull(result);
+            CollectionAssert.AllItemsAreInstancesOfType(result, typeof(Skill));
+            CollectionAssert.AllItemsAreUnique(result);
+        }
+    }
+}
--- FusionManager/Models/SkillModel.cs
+++ FusionManager/Models/SkillModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CsvHelper;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,39 @@ namespace FusionManager.Models
 {
     public class SkillModel
     {
+        List<Skill> skillList;
 
+        public SkillModel(StreamReader reader)
+        {
+            skillList = BuildSkillList(reader);
+        }
+
+        public List<Skill> GetSkillList()
+        {
+            return skillList;
+        }
+
+        protected List<Skill> BuildSkillList(StreamReader reader)
+        {
+            List<Skill> list = new List<Skill>();
+
+            using (TextReader textReader = reader)
+            {
+                var csv = new CsvReader(textReader);
+                while (csv.Read())
+                {
+                    Skill skill = new Skill();
+                    skill.CanPassDown = (csv.GetField<string>("Heritable") == "X") ? true : false;
+                    skill.Cost = csv.GetField<string>("Cost");
+                    skill.Description = csv.GetField<string>("Description");
+                    skill.Name = csv.GetField<string>("Skill");
+                    skill.SkillType = (SkillInheritanceType)Enum.Parse(typeof(SkillInheritanceType), csv.GetField<string>("InheritanceType")); 
+                    skill.Type = csv.GetField<string>("Type");                    
+                    list.Add(skill);
+                }
+            }
+
+            return list;
+        }
     }
 }
