c3ed91e10c2c17166df52753a3d59baa8fac96a7
lchesley <lisachesley@gmail.com>
2016-08-18 16:07:22 -0400

    Add skill colours and listing
    Added skill colours in CSS and listing, enum helper for skill
    inheritance type.

--- FusionManager/Models/SkillInheritanceType.cs
+++ FusionManager/Models/SkillInheritanceType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,18 +9,31 @@ namespace FusionManager.Models
 {
     public enum SkillInheritanceType
     {
+        [Description("Ph")]
         Physical,
+        [Description("F")]
         Fire,
+        [Description("I")]
         Ice,
+        [Description("E")]
         Electricity,
+        [Description("W")]
         Wind,
+        [Description("L")]
         Light,
+        [Description("D")]
         Dark,
+        [Description("A")]
         Almighty,
+        [Description("St")]
         Status,
+        [Description("R")]
         Recovery,
+        [Description("Su")]
         Support,
+        [Description("P")]
         Passive,
+        [Description("N")]
         Navigator
-    }
+    }  
 }
